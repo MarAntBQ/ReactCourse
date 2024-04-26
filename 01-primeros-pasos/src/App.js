cd4b6a51a39684d6d43ab8894810feb068cf30e0
MarAntBQ <github@marantbq.dev>
2024-04-25 22:23:03 -0500

    Finish my first component test

--- 01-primeros-pasos/src/App.js
+++ 01-primeros-pasos/src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import MiComponente from './MiComponente';
 
 function App() {
   return (
@@ -9,14 +10,8 @@ function App() {
         <p>
           Welcome to my first App
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      {/* Import Component */}
+      <MiComponente />      
       </header>
     </div>
   );
---
+++ 01-primeros-pasos/src/MiComponente.js
@@ -0,0 +1,21 @@
+// Import React Modules
+import React, {Fragment} from "react";
+
+// Component Function
+const MiComponente = () => {
+
+  return (
+  <div className="mi-component">
+  <hr/>
+  <p>Este es mi primer componente</p>
+  <ul>
+    <li>React</li>
+    <li>Angular</li>
+    <li>Vue</li>
+  </ul>
+  </div>
+)
+};
+
+// Export
+export default MiComponente
