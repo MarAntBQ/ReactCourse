20024f5e84915e4797f9b24c897d9a2407df8e66
MarAntBQ <github@marantbq.dev>
2024-04-30 21:16:48 -0500

    Double Click event

--- 01-primeros-pasos/src/EventosComponente.js
+++ 01-primeros-pasos/src/EventosComponente.js
@@ -3,19 +3,23 @@ import React from 'react'
 export const EventosComponente = () => {
   const isClicked = (e, userName) => {
     console.log(e)
-    console.log(`Hi I'm an event on click for you ${userName}`)
+    alert(`Hi I'm an event on click for you ${userName}`)
   };
   function dobleClick(e) {
     console.log(e)
-    console.log(`You clicked twice`)
+    alert(`You clicked twice`)
   }
   return (
     <div>
       <h1>Events on React</h1>
-      {/* Click Events */}
-      <button onClick={ e => isClicked(e, "Marco Antonio") }>Click Here # 1</button>
-      {/* Click Events doble */}
-      <button onDoubleClick={ dobleClick }>Click Here # 2</button>
+      <p>
+        {/* Click Events */}
+        <button onClick={ e => isClicked(e, "Marco Antonio") }>Click Here # 1</button>
+      </p>
+      <p>
+        {/* Click Events doble */}
+        <button onDoubleClick={ dobleClick }>Click Here # 2</button>
+      </p>
     </div>
   )
 }
