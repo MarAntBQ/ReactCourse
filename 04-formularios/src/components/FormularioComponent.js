dedd4c7805fb2ad7884cfbf59173edd1b28b1a0a
MarAntBQ <github@marantbq.dev>
2024-05-09 00:17:18 -0500

    Finish form and changing data

--- 04-formularios/src/components/FormularioComponent.js
+++ 04-formularios/src/components/FormularioComponent.js
@@ -12,43 +12,59 @@ export const FormularioComponent = () => {
       nombre: datos.nombre.value,
       apellido: datos.apellido.value,
       genero: datos.genero.value,
-      biografia: datos.bio.value,
+      biografia: datos.biografia.value,
+      enviar: datos.enviar.value
     }
     console.log(usuario);
     setUsuario(usuario)
   }
 
-  if (Object.keys(usuario).length === 0) {
-    return (
-      <div>
-        <h1>Forms with React.js</h1>
-        <form onSubmit={conseguirDatosForm}>
-          <input type='text'
-            name="nombre"
-            placeholder='Nombre'/>
-          <input type='text'
-            name="apellido"
-            placeholder='Apellido'/>
-          <select name="genero">
-            <option value="hombre">Hombre</option>
-            <option value="mujer">Mujer</option>
-          </select>
-          <textarea 
-            name="bio"
-            placeholder='Biografia'></textarea>
-          <input type='submit' value="Enviar"/>
-        </form>
-      </div>
-    )
-  } else {
-    return(
-      <div className='label'>
+  const cambiarDatos = e => {
+    console.log(e.target.name);
+    let inputName = e.target.name;
+    setUsuario(estado_previo => ({
+        ...estado_previo,
+        [inputName]: e.target.value
+      })
+    );
+  }
+
+  return (
+    <div>
+      <h1>Forms with React.js</h1>
+      { usuario.enviar &&
+        (<div className='label'>
         <h1>Datos del Usuario</h1>
         <ol><strong>Nombre:</strong> {usuario.nombre}</ol>
         <ol><strong>Apellido:</strong> {usuario.apellido}</ol>
         <ol><strong>Genero:</strong> {usuario.genero}</ol>
         <ol><strong>Biografia:</strong> {usuario.biografia}</ol>
-      </div>
-    )
-  }
+      </div>)
+      }
+      <form onSubmit={conseguirDatosForm}>
+        <input type='text'
+          name="nombre"
+          placeholder='Nombre'
+          onChange={cambiarDatos}
+        />
+        <input type='text'
+          name="apellido"
+          placeholder='Apellido'
+          onChange={cambiarDatos}
+        />
+        <select name="genero"
+          onChange={cambiarDatos}
+        >
+          <option value="hombre">Hombre</option>
+          <option value="mujer">Mujer</option>
+        </select>
+        <textarea 
+          name="biografia"
+          placeholder='Biografia'
+          onChange={cambiarDatos}
+        ></textarea>
+        <input type='submit' value="Enviar" name='enviar'/>
+      </form>
+    </div>
+  )
 }
