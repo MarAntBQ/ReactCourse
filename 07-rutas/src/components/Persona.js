bd9c418c59495741fa62b1b74029ebc6ab5fa768
MarAntBQ <github@marantbq.dev>
2024-05-17 17:58:27 -0500

    Hook useNavigate & useRef as something extra I researched for focus

--- 07-rutas/src/components/Persona.js
+++ 07-rutas/src/components/Persona.js
@@ -1,12 +1,41 @@
-import React from 'react'
-import { useParams } from 'react-router'
+import React, {useRef} from 'react'
+import { useNavigate, useParams } from 'react-router'
 
 export const Persona = () => {
   const {nombre, apellido} = useParams();
-  //console.log(parametros)
+  const navegar = useNavigate();
+  const nombreRef = useRef(null);
+  const apellidoRef = useRef(null);
+
+  const sendData = e => {
+    e.preventDefault();
+
+    let target = e.target;
+    let nombre = target.nombre.value;
+    let apellido = target.apellido.value;
+
+    if (!nombre) {
+      nombreRef.current.focus();
+      return;
+    }
+    if (!apellido) {
+      apellidoRef.current.focus();
+      return;
+    }
+
+    let url = `/persona/${nombre}/${apellido}`;
+    navegar(url);
+  }
+
   return (
     <div>
-      {!nombre && <h1>No hay ninguna persona que mostrar</h1>}
+      {!nombre && (<><h1>No hay ninguna persona que mostrar</h1>
+      <form onSubmit={sendData}>
+        <input type="text" name="nombre" ref={nombreRef}/>
+        <input type="text" name="apellido" ref={apellidoRef}/>
+        <input type="submit" name="enviar" value="Enviar"/>
+      </form></>
+      )}
       {nombre && <h1>Página de Persona: {nombre} {apellido}</h1>}
     </div>
   )
