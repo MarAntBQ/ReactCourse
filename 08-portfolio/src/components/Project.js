823a205c28dce56ff5a91e6c642a3c55b2459d43
MarAntBQ <github@marantbq.dev>
2024-05-18 23:08:55 -0500

    Page for project details

---
+++ 08-portfolio/src/components/Project.js
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from 'react'
+import { Link, useNavigate, useParams } from 'react-router-dom'
+import {projects} from '../data/projects'
+
+export const Project = () => {
+  
+  const [project, setProject] = useState({});
+  const params = useParams();
+  const navegar = useNavigate();
+
+  useEffect(() => {
+    let project = projects.filter(project => project.id === params.id);
+    if (project.length === 0) {
+      navegar('/portfolio');
+    } else {
+      setProject(project[0]);
+    }
+  }, []); // Execute only oncef
+
+  return (
+    <div className='path-project'>
+      <div className='project__intro'>
+        <h1>{project.name}</h1>
+        <hr/>
+        <img src={"/img/projects/"+params.id+".jpg"} alt={params.id+" image"}/>
+        <hr/>
+        <p>{project.description}</p>
+        <ul>
+          <li><strong>Tech Stack:</strong> {project.techstack}</li>
+          <li><strong>Date:</strong> {project.date}</li>
+        </ul>
+        <Link to={project.url} className='btn btn--center' target='_blank'>Go to Project</Link>
+      </div>
+    </div>
+  )
+}
--- 08-portfolio/src/router/MyRoutes.js
+++ 08-portfolio/src/router/MyRoutes.js
@@ -7,6 +7,7 @@ import { Resume } from '../components/Resume';
 import { Services } from '../components/Services';
 import { Portfolio } from '../components/Portfolio';
 import { Contact } from '../components/Contact';
+import { Project } from '../components/Project';
 import { Error404 } from '../components/Error404';
 import { Footer } from '../layout/Footer';
 
@@ -23,6 +24,8 @@ export const MyRoutes = () => {
         <Route path="/services" element={<Services/>}></Route>
         <Route path="/portfolio" element={<Portfolio/>}></Route>
         <Route path="/contact" element={<Contact/>}></Route>
+        <Route path="/project/:id" element={<Project/>}></Route>
+        <Route path="/project/" element={<Project/>}></Route>
         <Route path="*" element={<Error404/>}></Route>
       </Routes>
       </div>
