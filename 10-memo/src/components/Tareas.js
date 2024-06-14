1d7f88033c093789a77fd6fb864a419814d70f80
MarAntBQ <github@marantbq.dev>
2024-06-13 22:59:34 -0500

    Part # 2 of hook useMemo

--- 10-memo/src/components/Tareas.js
+++ 10-memo/src/components/Tareas.js
@@ -1,16 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export const Tareas = () => {
 
+  const [tareas, setTareas] = useState([]);
+
+  const guardarTareas = e => {
+    e.preventDefault();
+    let tareas_actualizadas = [...tareas, e.target.descripcion.value];
+
+    setTareas(tareas_actualizadas);
+    console.log(tareas);
+  }
   return (
     <div className='tareas-container'>
       <h1>Mis Tareas</h1>
-      <form>
-        <input type='text' title='descripcion' placeholder='Describe la tarea'/>
+      <form onSubmit={guardarTareas}>
+        <input type='text' name='descripcion' placeholder='Describe la tarea'/>
         <input type='submit' value="Guardar"/>
       </form>
       <hr/>
       <h2>Lista de tareas</h2>
+      <ul>
+        {
+          tareas.map((tarea, index) => (
+            <li key={index}>{tarea}</li>
+          ))
+        }
+      </ul>
     </div>
   )
 }
