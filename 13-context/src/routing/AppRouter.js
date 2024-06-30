2f10e4456a284fd15c5f06b1ef4f745cc3ed94ac
MarAntBQ <github@marantbq.dev>
2024-06-29 21:13:30 -0500

    feat: Add new pages to the project

---
+++ 13-context/src/routing/AppRouter.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Routes, Route, NavLink, BrowserRouter} from 'react-router-dom';
+
+export const AppRouter = () => {
+  return (
+    <BrowserRouter>
+        <Routes>
+            
+        </Routes>
+    </BrowserRouter>
+  )
+}
