3a1a067e86cd5379d15a1b1159b7de5ea18ad6fe
MarAntBQ <github@marantbq.dev>
2024-07-11 21:47:40 -0500

    Finish lesson with custom Hook

--- 14-more-hooks/src/components/PruebasCustom.js
+++ 14-more-hooks/src/components/PruebasCustom.js
@@ -2,13 +2,19 @@ import React from 'react'
 import { useMayus } from '../hooks/useMayus'
 
 export const PruebasCustom = () => {
-    const {mayusculas, minusculas, concatenar } = useMayus("marantbq.dev");
+    const {estado, mayusculas, minusculas, concatenar } = useMayus("MarAntBQ.dev");
 
-    console.log(minusculas());
   return (
     <div>
         <h1>Testing customized Components</h1>
-        <p>Test</p>
+        <p>{estado}</p>
+        <p>
+            <a onClick={ mayusculas }>Mayus</a>
+            &nbsp;|&nbsp;
+            <a onClick={ minusculas }>Minus</a>
+            &nbsp;|&nbsp;
+            <a onClick={ e=> concatenar(" is my web") }>concatenar</a>
+          </p>
     </div>
   )
 }
--- 14-more-hooks/src/hooks/useMayus.js
+++ 14-more-hooks/src/hooks/useMayus.js
@@ -1,18 +1,23 @@
+import { useState } from 'react'
+
 export const useMayus = (texto) => {
+
+    const [miTexto, setMiTexto] = useState(texto);
    
     const mayusculas = () => {
-        return texto.toUpperCase();
+        setMiTexto(texto.toUpperCase())
     }
     const minusculas = () => {
-        return texto.toLowerCase();
+        setMiTexto(texto.toLowerCase())
     }
     const concatenar = (added) => {
-        return texto + added;
+       setMiTexto(texto + added);
     }
 
     return {
+        estado: miTexto,
         mayusculas,
         minusculas,
-        concatenar
+        concatenar,
     };
 }
