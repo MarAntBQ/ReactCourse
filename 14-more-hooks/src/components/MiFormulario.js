5f2e25c1a8525e38429b2cdd2f88f09830e04efd
MarAntBQ <github@marantbq.dev>
2024-07-15 22:49:36 -0500

    Create function for onChange input values

--- 14-more-hooks/src/components/MiFormulario.js
+++ 14-more-hooks/src/components/MiFormulario.js
@@ -29,18 +29,27 @@ export const MiFormulario = () => {
 
         setFormulario(curso);
     }
+
+    const cambiado = ({target}) => {
+        const {name, value} = target;
+        setFormulario({
+            ...formulario,
+            [name]: value
+        })
+    }
+
   return (
     <div>
         <h1>Form</h1>
         <p>Form to store data of a course</p>
-        <p><strong>Saved Course:</strong> </p>
-        <pre>{JSON.stringify(formulario)}</pre>
+        <p><strong>Saved Course:</strong> {formulario.title} </p>
+        <pre className='code'>{JSON.stringify(formulario)}</pre>
         <form className='my-form' onSubmit={sent}>
-            <input type='text' name="title" placeholder='Title:'/>
-            <input type='number' name="year" placeholder='Published Year:'/>
-            <textarea name="description" placeholder='Description'/>
-            <input type='text' name="author" placeholder='Author:'/>
-            <input type='email' name="email" placeholder='E-mail Address:'/>
+            <input type='text' name="title" placeholder='Title:' onChange={cambiado}/>
+            <input type='number' name="year" placeholder='Published Year:' onChange={cambiado}/>
+            <textarea name="description" placeholder='Description' onChange={cambiado}/>
+            <input type='text' name="author" placeholder='Author:' onChange={cambiado}/>
+            <input type='email' name="email" placeholder='E-mail Address:' onChange={cambiado}/>
             <button>Send</button>
         </form>
     </div>
