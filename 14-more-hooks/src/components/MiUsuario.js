e39bcac523daf3d7317327f3e09cb309cd778fff
MarAntBQ <github@marantbq.dev>
2024-07-18 01:18:05 -0500

    Adding a loading effect

--- 14-more-hooks/src/components/MiUsuario.js
+++ 14-more-hooks/src/components/MiUsuario.js
@@ -1,19 +1,27 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 export const MiUsuario = () => {
 
     const [usuario,setUsuario] = useState({
-        datos: null
+        datos: null,
+        cargando: true
     });
 
     const getUsuario = async(url) => {
-        const peticion = await fetch(url);
-        const {data} = await peticion.json();
-
-        console.log(data);
         setUsuario({
-            datos: data
-        })
+            ...usuario,
+            cargando: true
+        });
+        setTimeout(async() => {
+            const peticion = await fetch(url);
+            const {data} = await peticion.json();
+    
+            console.log(data);
+            setUsuario({
+                datos: data,
+                cargando: false
+            })
+        }, 2000)
     }
 
     const getId = e => {
@@ -24,11 +32,16 @@ export const MiUsuario = () => {
         getUsuario(url);
     }
 
+    useEffect(() => {
+        getUsuario('https://reqres.in/api/users/1');
+    }, [])
+
   return (
     <div>
         <h1>Mi Usuario</h1>
         <p>User Data</p>
-        <p>{usuario?.datos?.first_name} - {usuario?.datos?.last_name}</p>
+        <p>{usuario.cargando ? "Cargando" : ''}</p>
+        <p>{usuario?.datos?.first_name} {usuario?.datos?.last_name}</p>
         <input type="number" name="id" onChange={getId}/>
     </div>
   )
