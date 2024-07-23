cd12efa394c11be26a3ea7f618d01236a18f5aab
MarAntBQ <github@marantbq.dev>
2024-07-22 20:43:46 -0500

    Create a new custom Hook called: useAjax

--- 14-more-hooks/src/components/MiUsuario.js
+++ 14-more-hooks/src/components/MiUsuario.js
@@ -1,47 +1,23 @@
 import React, { useEffect, useState } from 'react'
+import { useAjax } from '../hooks/useAjax'
 
 export const MiUsuario = () => {
 
-    const [usuario,setUsuario] = useState({
-        datos: null,
-        cargando: true
-    });
-
-    const getUsuario = async(url) => {
-        setUsuario({
-            ...usuario,
-            cargando: true
-        });
-        setTimeout(async() => {
-            const peticion = await fetch(url);
-            const {data} = await peticion.json();
-    
-            console.log(data);
-            setUsuario({
-                datos: data,
-                cargando: false
-            })
-        }, 2000)
-    }
+    const [url, setUrl] = useState("https://reqres.in/api/users/1");
+    const {datos, cargando} = useAjax(url)
 
     const getId = e => {
         console.log(e.target.value)
         let id = parseInt(e.target.value);
-        let url = `https://reqres.in/api/users/${id}`;
-
-        getUsuario(url);
+        setUrl(`https://reqres.in/api/users/${id}`);
     }
-
-    useEffect(() => {
-        getUsuario('https://reqres.in/api/users/1');
-    }, [])
 
   return (
     <div>
         <h1>Mi Usuario</h1>
         <p>User Data</p>
-        <p>{usuario.cargando ? "Cargando" : ''}</p>
-        <p>{usuario?.datos?.first_name} {usuario?.datos?.last_name}</p>
+        <p>{cargando ? "Cargando" : ''}</p>
+        <p>{datos?.first_name} {datos?.last_name}</p>
         <input type="number" name="id" onChange={getId}/>
     </div>
   )
