a085532a55d2d6f367bc305e62bd01be31852ea8
MarAntBQ <github@marantbq.dev>
2024-08-01 22:06:03 -0500

    Testing a port

--- 15-api-rest-node/index.js
+++ 15-api-rest-node/index.js
@@ -1,1 +1,14 @@
 console.log('Hello, world from Node.js!');
+
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Servidor funcionando en el puerto 3000\n');
+});
+
+const PORT = 3000;
+server.listen(PORT, () => {
+  console.log(`Servidor escuchando en el puerto ${PORT}`);
+});
