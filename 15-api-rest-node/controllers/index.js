0d10e6e2b37f8fef462c92910705e5258532f4c9
MarAntBQ <github@marantbq.dev>
2024-08-12 20:55:37 -0500

    feat: Add new endpoint for testing
    The code changes add a new endpoint `/testing` to the API. This endpoint returns a JSON response with information about the course, author, and URL. This endpoint can be used for testing purposes.
    
    Note: The commit message follows the convention of starting with a type (feat for feature) and providing a concise and descriptive message.

--- 15-api-rest-node/controllers/index.js
+++ 15-api-rest-node/controllers/index.js
@@ -4,3 +4,12 @@ exports.getHome = (req, res, next) => {
 exports.getAbout = (req, res, next) => {
     res.send('About Endpoint');
 }
+
+exports.getTesting = (req, res, next) => {
+    //res.send('Testing Endpoint');
+    return res.status(200).send({
+        course: "Node.js",
+        author: "MarAntBQ",
+        url: "https://marantbq.dev"
+    });
+}
--- 15-api-rest-node/routes/index.js
+++ 15-api-rest-node/routes/index.js
@@ -6,5 +6,6 @@ const router = express.Router();
 
 router.get('/', controller.getHome);
 router.get('/about', controller.getAbout);
+router.get('/testing', controller.getTesting);
 
 module.exports = router;
